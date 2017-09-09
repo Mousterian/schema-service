d7a1bff50d448537e86ea841aaf4d570a418c330
Mousterian <psandison@hotmail.com>
2017-09-09 23:57:11 +0100

    Placeholder in controller for returning the config to initialise angular schema form.

--- src/main/java/com/kasei/schema/SchemaController.java
+++ src/main/java/com/kasei/schema/SchemaController.java
@@ -1,10 +1,12 @@
 package com.kasei.schema;
 
 
-import com.kasei.schema.model.Schema;
+import com.kasei.schema.model.jdbc.Schema;
+import com.kasei.schema.model.schemaform.SchemaFormData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.support.JdbcUtils;
 import org.springframework.jdbc.support.MetaDataAccessException;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +35,14 @@ public class SchemaController {
             e.printStackTrace();
         }
         return schema;
+    }
+
+    @RequestMapping("/schemaformdata/{schema}/{table}")
+    public SchemaFormData getSchemaFormData(@PathVariable String schema, @PathVariable String table) {
+        SchemaFormData schemaFormData = null;
+        System.out.print("Got schema: " + schema + ", and table: " + table);
+
+        return schemaFormData;
     }
 
 }
